83f5f4af0e0dac681982e5d9e242e127591acf32
PedroPiuma <lpap1000@gmail.com>
2022-01-16 22:39:43 -0300

    16/01

---
+++ teste.js
@@ -0,0 +1,28 @@
+// true
+// false
+
+// AND (&&)
+console.log(true && false);
+
+// Tabela da AND
+/*
+true && true   => true
+true && false  => false
+false && false => true
+*/
+
+// OR (||)
+console.log(true || false);
+// Tabela da OR
+/*
+true || true   => true
+true || false  => true
+false || false => false
+*/
+
+// NOT (!)
+// Tabela do NOT
+// ! true => false
+// ! false => true
+console.log(!true);
+console.log(!false);
