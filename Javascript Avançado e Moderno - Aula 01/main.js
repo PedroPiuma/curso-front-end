c1f608a7aa6b2984aa63ddde0ab15b9bc6ae1788
PedroPiuma <lpap1000@gmail.com>
2022-01-18 00:42:43 -0300

    18/01

---
+++ Javascript Avançado e Moderno - Aula 01/main.js
@@ -0,0 +1,19 @@
+const value = 'str'
+
+console.log(typeof value)
+console.log(Boolean(value))
+
+/* 
+Boolean
+String
+Number
+*/
+
+const value1 = ''
+if (value1) {
+    console.log('Condição resolve para true')
+} else {
+    console.log('Condição resolve para false')
+}
+
+console.log(Boolean(value))
